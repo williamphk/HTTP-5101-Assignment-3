0af7f9065b62104f859d0642215a2f35ef1dccca
hongkanpoon <willphk@outlook.com>
2023-04-02 16:56:10 -0400

    Added JavaScript and Client Side Validation when Creating a Teacher

---
+++ HTTP 5101 Assignment 3/Scripts/teacher.js
@@ -0,0 +1,46 @@
+﻿function ValidateTeacher() {
+
+	var IsValid = true;
+	var ErrorMsg = "";
+	var ErrorBox = document.getElementById("ErrorBox");
+	var TeacherFname = document.getElementById('TeacherFname').value;
+	var TeacherLname = document.getElementById('TeacherLname').value;
+	var TeacherEmployeeNumber = document.getElementById('TeacherEmployeeNumber').value;
+	var TeacherHireDate = document.getElementById('TeacherHireDate').value;
+	var TeacherSalary = document.getElementById('TeacherSalary').value;
+
+	//First Name is two or more characters
+	if (TeacherFname.length < 2) {
+		IsValid = false;
+		ErrorMsg += "First Name Must be 2 or more characters.<br>";
+	}
+	//Last Name is two or more characters
+	if (TeacherLname.length < 2) {
+		IsValid = false;
+		ErrorMsg += "Last Name Must be 2 or more characters.<br>";
+	}
+	//Email is valid pattern
+	if (!TeacherEmployeeNumber) {
+		IsValid = false;
+		ErrorMsg += "Please Enter a Employee Number.<br>";
+	}
+	if (!TeacherHireDate) {
+		IsValid = false;
+		ErrorMsg += "Please Select a Hire Date.<br>";
+	}
+	if (!TeacherSalary) {
+		IsValid = false;
+		ErrorMsg += "Please Enter a Salary.<br>";
+	}
+
+	if (!IsValid) {
+		ErrorBox.style.display = "block";
+		ErrorBox.innerHTML = ErrorMsg;
+	} else {
+		ErrorBox.style.display = "none";
+		ErrorBox.innerHTML = "";
+	}
+
+
+	return IsValid;
+}
